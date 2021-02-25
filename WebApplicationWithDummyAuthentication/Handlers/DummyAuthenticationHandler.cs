9deabfcf8dd1232b70c4f3a9cc1b8d7f9f01f010
Ilya Valchanka <Ilya.Valchanka@thomsonreuters.com>
2021-02-26 00:09:00 +0300

    Added more comments.

--- WebApplicationWithDummyAuthentication/Handlers/DummyAuthenticationHandler.cs
+++ WebApplicationWithDummyAuthentication/Handlers/DummyAuthenticationHandler.cs
@@ -30,6 +30,19 @@ namespace WebApplicationWithDummyAuthentication.Handlers
             }
 
             // Create a ClaimsPrincipal from your header.
+            // Claim is a key-value pair. We can use FirstName or EmailAddress as a key.
+            // User has the role X means that we have one claim. We have predefined set of claim types from System.Security.Claims.ClaimTypes,
+            // but you can create your own claim.
+
+            // The next important thing is Identity. You can think of the Identity as about a document which
+            // has a list of claims and an authentication type. In Core MVC we use System.Security.Claims.ClaimsIdentity.
+
+            // The next important thing which is located above the Indentity is Principal.
+            // You can think about the Principal as about a user itself.
+            // The Principal can contain more than one Identity associated with a user.
+            // You can retrieve all claims of all identities by using the Principal.
+            // If you have more than one identity then you can manage a user access to different parts
+            // of a web application.
             var claims = new[]
             {
                 new Claim(ClaimTypes.NameIdentifier, "My Name")
