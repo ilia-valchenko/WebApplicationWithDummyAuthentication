07d28dff92fd3d4247fdc219526d60c2a3d21a83
Ilya Valchanka <Ilya.Valchanka@thomsonreuters.com>
2021-02-26 11:54:40 +0300

    Added more comments.

--- WebApplicationWithDummyAuthentication/Startup.cs
+++ WebApplicationWithDummyAuthentication/Startup.cs
@@ -11,6 +11,11 @@ namespace WebApplicationWithDummyAuthentication
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        // ASP .NET Core has built-in dependency injection functionality.
+        // It has built-in IoC container.
+        // If you want more features such as auto-registration, scanning, interceptors, or decorators
+        // then you may replace built-in IoC container with a third party container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
